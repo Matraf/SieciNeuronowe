656d8d2174bc1e471436fe88240718c75ec0c635
Matraf <sanandreas180@wp.pl>
2022-03-30 18:57:38 +0200

    fix cw4

--- cw4/metodaGradientu.py
+++ cw4/metodaGradientu.py
@@ -1,70 +1,73 @@
 import random
 
 c = 0.01
-eps = 0.0000001
+E = 0.0000001
 
 
-def derivative_x(x, y):
+def pochodna_x(x, y, z):
     return 4 * x - 2 * y - 2
 
 
-def derivative_y(x, y, z):
+def pochodna_y(x, y, z):
     return 4 * y - 2 * x - 2 * z
 
 
-def derivative_z(y, z):
+def pochodna_z(x, y, z):
     return 2 * z - 2 * y
 
 
-def derivative_x2(x):
+def pochodna_x2(x, y):
     return 12 * x ** 3 + 12 * x ** 2 - 24 * x
 
 
-def derivative_y2(y):
+def pochodna_y2(x, y):
     return 24 * y - 24
 
 
-def first_function(x_old, y_old, z_old):
-    x_new, y_new, z_new = 0, 0, 0
+x_old, y_old, z_old = random.random(), random.random(), random.random()
+
+max = 0
+x_new, y_new, z_new = 0, 0, 0
+
+while True:
+    x_new = x_old - c * pochodna_x(x_old, y_old, z_old)
+    y_new = y_old - c * pochodna_y(x_old, y_old, z_old)
+    z_new = z_old - c * pochodna_z(x_old, y_old, z_old)
+
     max = 0
-    while max < eps:
-        x_new = x_old - c * derivative_x(x_old, y_old)
-        y_new = y_old - c * derivative_y(x_old, y_old, z_old)
-        z_new = z_old - c * derivative_z(y_old, z_old)
+    if abs(x_new - x_old) > max:
+        max = abs(x_new - x_old)
+    if abs(y_new - y_old) > max:
+        max = abs(y_new - y_old)
+    if abs(z_new - z_old) > max:
+        max = abs(z_new - z_old)
+    if max < E:
+        break
 
-        max = 0
-        if abs(x_new - x_old) > max:
-            max = abs(x_new - x_old)
-        if abs(y_new - y_old) > max:
-            max = abs(y_new - y_old)
-        if abs(z_new - z_old) > max:
-            max = abs(z_new - z_old)
+    x_old, y_old, z_old = x_new, y_new, z_new
 
-        x_old, y_old, z_old = x_new, y_new, z_new
-    print(x_new, y_new, z_new,
-          2 * x_new ** 2 + 2 * y_new ** 2 + z_new ** 2 - 2 * x_new * y_new - 2 * y_new * z_new - 2 * x_new + 3)
+print(x_new, y_new, z_new,
+      2 * x_new ** 2 + 2 * y_new ** 2 + z_new ** 2 - 2 * x_new * y_new - 2 * y_new * z_new - 2 * x_new + 3)
 
+# druga funkcja
+x_old, y_old = random.random(), random.random()
+# x_old, y_old = 2, 3
 
-def second_function(x_old, y_old):
+max = 0
+x_new, y_new = 0, 0
+
+while True:
+    x_new = x_old - c * pochodna_x2(x_old, y_old)
+    y_new = y_old - c * pochodna_y2(x_old, y_old)
+
     max = 0
-    x_new, y_new = 0, 0
-    while max < eps:
-        x_new = x_old - c * derivative_x2(x_old)
-        y_new = y_old - c * derivative_y2(y_old)
+    if abs(x_new - x_old) > max:
+        max = abs(x_new - x_old)
+    if abs(y_new - y_old) > max:
+        max = abs(y_new - y_old)
+    if max < E:
+        break
 
-        max = 0
-        if abs(x_new - x_old) > max:
-            max = abs(x_new - x_old)
-        if abs(y_new - y_old) > max:
-            max = abs(y_new - y_old)
+    x_old, y_old = x_new, y_new
 
-        x_old, y_old = x_new, y_new
-
-    print(x_new, y_new,
-          3 * x_new ** 4 + 4 * x_new ** 3 - 12 * x_new ** 2 + 12 * y_new ** 2 - 24 * y_new)
-
-
-if __name__ == '__main__':
-    x, y, z = random.random(), random.random(), random.random()
-    first_function(x, y, z)
-    second_function(x, y)
+print(x_new, y_new, 3 * x_new ** 4 + 4 * x_new ** 3 - 12 * x_new ** 2 + 12 * y_new ** 2 - 24 * y_new)
